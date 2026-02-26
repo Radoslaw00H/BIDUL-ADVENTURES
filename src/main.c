4da6ab80543278678d6d78964328efe850b83609
Radoslaw00H <holubowskir@gmail.com>
2026-02-26 18:47:23 +0100

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,14 @@
+// Bidul Adventures game
+// Main event loop
+
+#include "INITIALIZATION/init.h"
+
+#include <windows.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+int main(void) {
+    init();
+
+
+}
