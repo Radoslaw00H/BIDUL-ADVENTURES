cf084798b0bfdeef818772aebc249deddae989e3
Radoslaw00H <holubowskir@gmail.com>
2026-03-01 08:03:32 +0100

    Update LOGIC.h

--- src/LOGIC/LOGIC.h
+++ src/LOGIC/LOGIC.h
@@ -9,13 +9,16 @@
 #include <float.h>
 #include <assert.h>
 
-#define TARGET_FPS 165
-#define WORLD_WIDTH 256
-#define WORLD_HEIGHT 256
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
-#define MAX_ENTITIES 64
-#define MAX_PROJECTILES 256
+#define TARGET_FPS 165                 // Target frame rate in Hz
+#define WORLD_WIDTH 256                // World map width in tiles
+#define WORLD_HEIGHT 256               // World map height in tiles
+#define SCREEN_WIDTH 640               // Screen width in pixels
+#define SCREEN_HEIGHT 480              // Screen height in pixels
+#define MAX_ENTITIES 64                // Maximum enemies that can exist
+#define MAX_PROJECTILES 256            // Maximum bullets in flight
+#define MAX_TEXTURES 3                 // Maximum entity texture types
+#define TEXTURE_MAX_WIDTH 128          // Maximum texture width in pixels
+#define TEXTURE_MAX_HEIGHT 128         // Maximum texture height in pixels
 
 // 3D vector
 typedef struct {
@@ -46,6 +49,7 @@ typedef struct {
     double width;   // Render as rectangle
     double height;
     double bob_angle; // Animation bobbing
+    uint8_t type;   // 0=normal, 1=red aggressive, 2=boss
 } Entity;
 
 // Projectile (bullet)
@@ -58,6 +62,14 @@ typedef struct {
     uint8_t color_g;
     uint8_t color_b;
 } Projectile;
+
+// Texture for rendering entities with graphics
+typedef struct {
+    uint32_t *pixels;              // Pixel buffer in BGR format
+    int width;                      // Texture width in pixels
+    int height;                     // Texture height in pixels
+    int loaded;                     // 1 = loaded successfully, 0 = failed
+} Texture;
 
 // Raycast result
 typedef struct {
@@ -75,6 +87,8 @@ extern Entity *g_entities;
 extern int g_entity_count;
 extern Projectile *g_projectiles;
 extern int g_projectile_count;
+extern Texture g_textures[MAX_TEXTURES];   // Textures for normal, red, boss entities
+extern double g_spawn_timer;               // Timer for spawning new enemies every 5 seconds
 
 // Core functions
 void clock_init(void);
@@ -86,7 +100,10 @@ void game_cleanup(void);
 
 // Gameplay functions
 void player_shoot(void);
-void spawn_enemy(double x, double y, double z);
+void spawn_enemy(double x, double y, double z, uint8_t type);
+void spawn_normal_enemy(double x, double y, double z);
+void spawn_red_enemy(double x, double y, double z);
+void spawn_boss(double x, double y, double z);
 void handle_key_input(uint8_t key, int is_down);
 RayHit raycast(double x, double y, double z, double angle_yaw, double angle_pitch);
 void tick(void);
